ee49a08d921417e6a834b44a730e595171be6c17
saracen75 <saracen75@gmail.com>
2024-07-09 08:57:49 +0800

    audit wip

--- src/L1Vault.sol
+++ src/L1Vault.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
-
+// #q why import a "proxy file" instead of the actual IERC20.sol?
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -8,11 +8,12 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 /// @author Boss Bridge Peeps
 /// @notice This contract is responsible for locking & unlocking tokens on the L1 or L2
 /// @notice It will approve the bridge to move money in and out of this contract
-/// @notice It's owner should be the bridge
+/// @notice It's owner should be the bridge // @q is this enforced in the code, or is there any way to check if this is true?
 contract L1Vault is Ownable {
     IERC20 public token;
 
     constructor(IERC20 _token) Ownable(msg.sender) {
+        // #audit missing zero address check
         token = _token;
     }
 
